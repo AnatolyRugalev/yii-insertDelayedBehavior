8e9eb8587c230f80d431a213eb2e5672fd266dd6
Anatoly Rugalev <anatoly.rugalev@gmail.com>
2012-12-10 01:09:14 +0600

    Added some properties to manipulate insert behavior

--- protected/behaviors/InsertDelayedBehavior.php
+++ protected/behaviors/InsertDelayedBehavior.php
@@ -5,40 +5,38 @@
  * Github: http://github.com/AnatolyRugalev/yii-insertDelayedBehavior
  */ 
 class InsertDelayedBehavior extends CBehavior {
+    /**
+     * @var string|null name of function to call before save or null to not call
+     */
+    public $beforeSaveFunction = 'beforeSave';
+    /**
+     * @var string|null name of function to call after save or null to not call
+     */
+    public $afterSaveFunction = null;
+    /**
+     * @var bool true for native insert on insert delayed fail
+     */
+    public $onFailSimpleInsert = true;
 
     public function saveDelayed($runValidation=true,$attributes=null)
     {
         if(!$runValidation || $this->owner->validate($attributes))
-            return $this->owner->getIsNewRecord() ? $this->insertDelayed() : $this->owner->update($attributes);
+            return $this->owner->getIsNewRecord() ? $this->insertDelayed($attributes) : $this->owner->update($attributes);
         else
             return false;
     }
 
-    /**
-     * Inserts a row into the table based on this active record attributes.
-     * If the table's primary key is auto-incremental and is null before insertion,
-     * it will be populated with the actual value after insertion.
-     * Note, validation is not performed in this method. You may call {@link validate} to perform the validation.
-     * After the record is inserted to DB successfully, its {@link isNewRecord} property will be set false,
-     * and its {@link scenario} property will be set to be 'update'.
-     * @return boolean whether the attributes are valid and the record is inserted successfully.
-     * @throws CDbException if the record is not new
-     */
-    public function insertDelayed()
+    public function insertDelayed($attributes)
     {
         if(!$this->owner->getIsNewRecord())
             throw new CDbException(Yii::t('yii','The active record cannot be inserted to database because it is not new.'));
-        if($this->owner->beforeInsertDelayed())
+        if(is_null($this->beforeSaveFunction) || $this->owner->{$this->beforeSaveFunction}())
         {
             Yii::trace(get_class($this->owner).'.insertDelayed()','application.behaviors.InsertDelayedBehavior');
             $builder=$this->owner->getCommandBuilder();
             $table=$this->owner->getMetaData()->tableSchema;
-            /** @var $command CDbCommand */
-            /** @var $builder CDbCommandBuilder */
             $command=$builder->createInsertCommand($table,$this->owner->getAttributes());
             $command->text = preg_replace('#^INSERT #is', 'INSERT DELAYED ', $command->text, 1);
-            /** @var $table CDbTableSchema */
-            $table = $this->owner->tableSchema;
             if(preg_match('#INSERT DELAYED INTO `\w+` \((.*?)\) VALUES \((.*?)\)#is', $command->text, $matches))
             {
                 $fields = explode(',', $matches[1]);
@@ -57,6 +55,8 @@ class InsertDelayedBehavior extends CBehavior {
             else
             {
                 Yii::log(get_class($this->owner).'.insertDelayed() - Cannot insert because of query does not match a regular expression', CLogger::LEVEL_ERROR,'application.behaviors.InsertDelayedBehavior');
+                if($this->onFailSimpleInsert)
+                    return $this->owner->insert($attributes);
                 return false;
             }
             if($command->execute($execParams))
@@ -64,7 +64,7 @@ class InsertDelayedBehavior extends CBehavior {
                 $this->owner->afterInsertDelayed();
                 $this->owner->setIsNewRecord(false);
                 $this->owner->setScenario('update');
-                return true;
+                return is_null($this->afterSaveFunction) || $this->owner->{$this->afterSaveFunction}();
             }
         }
         return false;
