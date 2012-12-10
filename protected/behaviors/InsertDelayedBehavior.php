74cf56b6197bbec6f5f877d94cbae28fc3e3ba60
Anatoly Rugalev <anatoly.rugalev@gmail.com>
2012-12-10 11:45:34 +0600

    Update protected/behaviors/InsertDelayedBehavior.php

--- protected/behaviors/InsertDelayedBehavior.php
+++ protected/behaviors/InsertDelayedBehavior.php
@@ -3,7 +3,7 @@
  * This behavior allows to save model with INSERT DELAYED query
  * Author: Anatoly Rugalev <anatoly.rugalev@gmail.com>
  * Github: http://github.com/AnatolyRugalev/yii-insertDelayedBehavior
- * Version: 0.1
+ * Version: 0.1.1
  */ 
 class InsertDelayedBehavior extends CBehavior {
     /**
@@ -62,7 +62,6 @@ class InsertDelayedBehavior extends CBehavior {
             }
             if($command->execute($execParams))
             {
-                $this->owner->afterInsertDelayed();
                 $this->owner->setIsNewRecord(false);
                 $this->owner->setScenario('update');
                 return is_null($this->afterSaveFunction) || $this->owner->{$this->afterSaveFunction}();
